8a1e8049677c5e6528f08c8be8ab9dc5ab28fe8f
Fawaz Oyebode <103122756+fawazdevx@users.noreply.github.com>
2026-04-19 02:26:23 +0100

    feat: Add Rock paper scissors game

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -30,7 +30,17 @@ const mockWeb3Games = [
         tags: ["Racing", "PVP"],
         url: "https://jamesbachini.github.io/Stellar-Game-Studio/",
         players: 156
-    }
+    },
+    {
+        
+        id: "g4",
+        title: "Rock Paper Scissors",
+        author: "Nestlle Web3 Team",
+        image: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?w=600&h=400&fit=crop",
+        tags: ["Action", "Arcade"],
+        url: "https://rock-paper-scissors-web-ten.vercel.app//",
+        players: 320
+    },
 ];
 
 const Games = () => {
